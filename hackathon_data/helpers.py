3b99a1dfaea27718aa6bf311c5a28d51b31a0b77
chrisbirie <chris.birie@gmail.com>
2024-04-19 08:57:29 -0400

    added dates to filter

--- hackathon_data/helpers.py
+++ hackathon_data/helpers.py
@@ -2,7 +2,7 @@ from hackathon_data.codewars_api import get_user_data, get_challenge_data
 from hackathon_data.scoring import calculate_score
 from datetime import datetime 
 
-def prepare_results_data(user_data, scoring_data):
+def prepare_results_data(user_data, scoring_data, start_time, end_time):
     user_data = user_data[1:]  # Skips the first row (header)
     scoring_data = scoring_data[1:]  # Skips the first row (header)
     data = []
@@ -13,20 +13,24 @@ def prepare_results_data(user_data, scoring_data):
         # Get completed challenges data for the user
         completed_challenges = user_completed_challenges.get('data', [])
         for challenge in completed_challenges:
-            # Get challenge details from CodeWars API
-            challenge_info = get_challenge_data(challenge['id'])
-            # Extract relevant challenge data
-            challenge_name = challenge_info.get('name', '')
-            completed_languages = challenge.get('completedLanguages', [])
-            end_time = format_datetime(challenge.get('completedAt', ''))
-            rank = challenge_info.get('rank', {})
-            rank_name = rank.get('name','')
+            # Extract completedAt datetime and check if it falls within the specified range
+            completed_at = format_datetime(challenge.get('completedAt', ''))
+            completed_at_datetime = datetime.strptime(completed_at, "%Y-%m-%d %H:%M%S:%f")
+            if start_time <= completed_at_datetime <= end_time:
+                # Get challenge details from CodeWars API
+                challenge_info = get_challenge_data(challenge['id'])
+                # Extract relevant challenge data
+                challenge_name = challenge_info.get('name', '')
+                completed_languages = challenge.get('completedLanguages', [])
+                completed_at_time = format_datetime(challenge.get('completedAt', ''))
+                rank = challenge_info.get('rank', {})
+                rank_name = rank.get('name','')
 
-            # Calculate score for the challenge
-            score = calculate_score(rank_name, scoring_data)
-        
-            # Append data to excel_data list
-            data.append((user['username'], user['full_name'], challenge_name, ', '.join(completed_languages), end_time, rank_name, score))
+                # Calculate score for the challenge
+                score = calculate_score(rank_name, scoring_data)
+            
+                # Append data to excel_data list
+                data.append((user['username'], user['full_name'], challenge_name, ', '.join(completed_languages), completed_at_time, rank_name, score))
 
     
     return data
--- scripts/hackathon_scoring_script.py
+++ scripts/hackathon_scoring_script.py
@@ -1,6 +1,7 @@
 from hackathon_data.excel_utils import create_excel_workbook
 from hackathon_data.file_utils import load_user_data_from_url, load_scoring_data_from_url
 from hackathon_data.helpers import prepare_results_data
+from datetime import datetime
 
 def main():
     # URLs for scoring and usernames data
@@ -13,7 +14,9 @@ def main():
     # Load user data from URL
     user_data = load_user_data_from_url(usernames_url)
 
-    data = prepare_results_data(user_data, scoring_data)
+    start_time = datetime(2024, 4, 19, 9, 0, 0)
+    end_time = datetime(2024, 4, 19, 23, 59)
+    data = prepare_results_data(user_data, scoring_data, start_time, end_time)
 
     # Create Excel workbook
     filename = "data/hackathon_scores.xlsx"
